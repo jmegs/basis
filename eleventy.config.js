ef01169292e0827031ff2d837955a7f9c1b54f8a
John Meguerian <johnmeguerian@gmail.com>
2021-03-17 09:47:38 -0400

    create site scaffold

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+module.exports = function (config) {
+  return {
+    dir: {
+      input: 'src',
+      layouts: 'layouts',
+      includes: 'partials',
+      data: '../data'
+    }
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './helpers/shortcodes/**/*.js'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
