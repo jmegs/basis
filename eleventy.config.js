5266de4197087bee53c7e482cb3a3fc3198ce4d5
John Meguerian <johnmeguerian@gmail.com>
2021-03-17 12:11:03 -0400

    add custom liquid instance

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,15 @@
 module.exports = function (config) {
+  // set custom liquid instance
+  let liquidJs = require("liquidjs");
+  let options = {
+    extname: ".html",
+    dynamicPartials: true,
+    strict_filters: true,
+    root: ["./src/partials", "./src/layouts"]
+  };
+
+  config.setLibrary("liquid", liquidJs(options));
+  
   return {
     dir: {
       input: 'src',
